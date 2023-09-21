5f158c250e63b7c35a9b1cc5d799f208b842f40b
vishuiitd <32016849+vishuiitd@users.noreply.github.com>
2023-09-21 12:27:46 +0530

    Add files via upload

---
+++ Models/Consumption.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApi.Utility;
+
+namespace WebApi.Models
+{
+    public class Consumption
+    {
+        [Required]
+        public string Site { get; set; }
+        [Required]
+        [Range(0,11)]
+        public MonthEnum Month { get; set; }
+        [Required]
+        public float CoalConsumption { get; set; }
+        [Required]
+        public float BioMassConsumption { get; set; }
+        [Required]
+        public float ElectricityConsumption { get; set; }
+        [Required]
+        public float RoadLogisticIncomingConsumption { get; set; }
+        [Required]
+        public float RoadLogisticOutgoingConsumption { get; set; }
+    }
+
+}
---
+++ Models/ScopeEmission.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Models
+{
+    public class ScopeEmission
+    {
+        public float ScopeOneEmission { get; set; }
+        public float ScopeTwoEmission { get; set; }
+        public float ScopeThreeEmission { get; set; }
+    }
+}
