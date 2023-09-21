50306dc46fd518348a592a1bd6f3680e936ecd06
vishuiitd <32016849+vishuiitd@users.noreply.github.com>
2023-09-21 12:16:29 +0530

    Add files via upload

---
+++ Utilities/Constants.cs
@@ -0,0 +1,29 @@
+﻿namespace WebApi.Utility
+{
+    public class Constants
+    {
+        public static readonly float CoalEmissionFactor = 2411.43f;
+        public static readonly float BioMassEmissionFactor = 43.03576f;
+        public static readonly float ElectricityEmissionFactor = 193.38f;
+        public static readonly float IncomingDieselEmissionFactor =   73840.0f;
+        public static readonly float OutgoingDieselEmissionFactor = 17870.0f;
+
+    }
+
+
+    public enum MonthEnum
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+}
---
+++ Utilities/Util.cs
@@ -0,0 +1,54 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Utility
+{
+    public class Util
+    {
+        public static float CalculateTotalEmmision(Consumption consumption)
+        {
+            return CalculateScopeOneEmissions(consumption) + CalculateScopeTwoEmissions(consumption) + CalculateScopeTreeEmissions(consumption);
+        }
+        public static float CalculateScopeOneEmissions(Consumption consumption)
+        {
+            return CalculateCoalEmission(consumption.CoalConsumption) + CalculateBioMassEmission(consumption.BioMassConsumption);
+        }
+
+        public static float CalculateScopeTwoEmissions(Consumption consumption)
+        {
+            return CalculateElectricityEmission(consumption.ElectricityConsumption);
+        }
+
+        public static float CalculateScopeTreeEmissions(Consumption consumption)
+        {
+            return CalculateIncomingDieselEmission(consumption.RoadLogisticIncomingConsumption) + CalculateOutgoingDieselEmission(consumption.RoadLogisticOutgoingConsumption);
+        }
+
+        private static float CalculateIncomingDieselEmission(float roadLogisticIncomingConsumption)
+        {
+            return roadLogisticIncomingConsumption * Constants.IncomingDieselEmissionFactor;
+        }
+
+        private static float CalculateOutgoingDieselEmission(float roadLogisticOutgoingConsumption)
+        {
+            return roadLogisticOutgoingConsumption*Constants.OutgoingDieselEmissionFactor ;
+        }
+
+        private static float CalculateCoalEmission(float coalConsumption)
+        {
+            return coalConsumption * Constants.CoalEmissionFactor;
+        }
+
+        private static float CalculateBioMassEmission(float bioMassConsumption)
+        {
+            return bioMassConsumption * Constants.BioMassEmissionFactor;
+        }
+
+        private static float CalculateElectricityEmission(float electricityConsumption)
+        {
+            return electricityConsumption * Constants.ElectricityEmissionFactor;
+        }
+
+        
+
+    }
+}
